472519d9714c3198cbeb1d0255f620dd794f6ccb
hhyanghh <yanghh711@naver.com>
2024-02-07 11:58:42 +0900

    first commit

---
+++ LinkedList.mjs
@@ -0,0 +1,103 @@
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.count = 0;
+  }
+
+  printAll() {
+    let currentNode = this.head;
+    let text = "[";
+
+    while (currentNode != null) {
+      text += currentNode.data;
+      currentNode = currentNode.next;
+
+      if (currentNode != null) {
+        text += ", ";
+      }
+    }
+    text += "]";
+    console.log(text);
+  }
+
+  clear() {
+    this.head = null;
+    this.count = 0;
+  }
+
+  insertAt(index, data) {
+    if (index > this.count || index < 0) {
+      throw new Error("범위를 다시 확인해주세요.");
+    }
+    let newNode = new Node(data);
+
+    if (index === 0) {
+      // 가장 앞부분에 삽입하는 경우
+
+      newNode.next = this.head;
+      this.head = newNode;
+    } else {
+      let currentNode = this.head; // 초기화
+
+      for (let i = 0; i < index - 1; i++) {
+        currentNode = currentNode.next; // currentNode 이동
+      }
+      newData.next = currentNode.next; // 뒤를 먼저 연결
+      currentNode.next = newData; //앞으로 삽입
+    }
+    this.count++;
+  }
+
+  insertLast(data) {
+    this.insertAt(this.count, data);
+  }
+
+  deleteAt(index) {
+    if (index >= this.count || index < 0) {
+      throw new Error("제거할 수 없습니다.");
+    }
+
+    let currentNode = this.head;
+    if (index === 0) {
+      // head node 제거
+      let deletedNode = this.head;
+      this.head = this.head.next;
+      this.count--;
+      return deletedNode;
+    } else {
+      for (let i = 0; i < index - 1; i++) {
+        currentNode = currentNode.next;
+      }
+      let deletedNode = currentNode.next;
+      currentNode.next = currentNode.next.next;
+      this.count--;
+      return deletedNode;
+    }
+  }
+
+  deleteLast() {
+    return this.deleteAt(this.count - 1);
+  }
+
+  getNodeAt(index) {
+    if (index >= this.count || index < 0) {
+      throw new Error("범위를 확인해주세요.");
+    }
+
+    let currentNode = this.head;
+    for (let i = 0; i < index - 1; i++) {
+      currentNode = currentNode.next;
+    }
+
+    return currentNode;
+  }
+}
+
+export { Node, LinkedList };
