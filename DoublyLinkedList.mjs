8c60e5161c8dee439f9dfe034c7d2f5cba53e6f4
hhyanghh <yanghh711@naver.com>
2024-02-08 10:48:10 +0900

    큐 구현

--- DoublyLinkedList.mjs
+++ DoublyLinkedList.mjs
@@ -1,5 +1,5 @@
 class Node {
-  constructor(data, next = null) {
+  constructor(data, next = null, prev = null) {
     this.data = data;
     this.next = next;
     this.prev = prev;
---
+++ test_queue.mjs
@@ -0,0 +1,17 @@
+import { Queue } from "./Queue.mjs";
+
+let queue = new Queue();
+
+console.log("=== enqueue 세번 호출 ===");
+queue.enqueue(1);
+queue.enqueue(2);
+queue.enqueue(3);
+
+console.log(queue.front());
+
+console.log("=== dequeue() 네 번 호출 ===");
+
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+
+console.log(queue.isEmpty());
