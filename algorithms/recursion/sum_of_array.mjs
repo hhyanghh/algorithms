65d4fb5a76461e91a0df8dadb7ae9ebd6bc6ba5c
hhyanghh <yanghh711@naver.com>
2024-02-14 09:46:49 +0900

    재귀 - 배열의 합 구하기

---
+++ algorithms/recursion/sum_of_array.mjs
@@ -0,0 +1,9 @@
+// 배열의 합 구하기
+function sumArray(arr) {
+  if (arr.length === 1) return arr[0];
+  return sumArray(arr.slice(0, -1)) + arr[arr.length - 1];
+}
+
+let arr = [1, 2, 3, 4, 5];
+let sum = sumArray(arr);
+console.log(sum);
